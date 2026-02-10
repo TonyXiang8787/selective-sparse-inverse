f62460cb199fb2686a6b11104d661ae8553eefdb
Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>
2026-02-10 15:17:12 +0100

    full inv
    Signed-off-by: Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>

--- scripts/run.py
+++ scripts/run.py
@@ -1,3 +1,7 @@
+import numpy as np
+import scipy.linalg as sclin
+
+from selective_sparse_inverse.full_inverse import lu_inv_full
 from selective_sparse_inverse.graph_generation import produce_random_tree_matrix
 from selective_sparse_inverse.lu_decomposition import lu_no_pivot
 
@@ -8,6 +12,10 @@ def main():
     print(A)
     lu = lu_no_pivot(A)
     print(lu)
+    lu_inv = lu_inv_full(lu)
+    lu_inv_scipy = sclin.inv(A)
+    max_diff = np.max(np.abs(lu_inv - lu_inv_scipy))
+    print(f"Max absolute difference between lu_inv and scipy inv: {max_diff:.6e}")
 
 
 if __name__ == "__main__":
---
+++ src/selective_sparse_inverse/full_inverse.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def lu_inv_full(lu: np.ndarray):
+    lu_inv = lu.copy()
+    _lu_inv_inplace(lu_inv)
+    return lu_inv
+
+
+def _lu_inv_inplace(lu_inv: np.ndarray):
+    n = lu_inv.shape[0]
+    if n == 1:
+        lu_inv[0, 0] = 1 / lu_inv[0, 0]
+        return
+
+    #  recursive call
+    z = lu_inv[1:, 1:]
+    _lu_inv_inplace(z)
+
+    # calculate current pivot
+    a00 = lu_inv[0, 0]
+    l0 = lu_inv[1:, 0]
+    u0 = lu_inv[0, 1:]
+    lu_inv[0, 0] = 1 / a00 + 1.0 / a00 / a00 * (u0 @ z @ l0)
+    lu_inv[1:, 0] = -1.0 / a00 * (z @ l0)
+    lu_inv[0, 1:] = -1.0 / a00 * (u0 @ z)
