c008862566f17091c303a188a0c9d219d1e41a00
Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>
2026-02-10 15:04:42 +0100

    lu no pivot
    Signed-off-by: Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>

--- scripts/run.py
+++ scripts/run.py
@@ -1,11 +1,13 @@
 from selective_sparse_inverse.graph_generation import produce_random_tree_matrix
+from selective_sparse_inverse.lu_decomposition import lu_no_pivot
 
 
 def main():
     n = 5
     A = produce_random_tree_matrix(n, seed=42)
     print(A)
-    print(A.shape)  # Should be (n, n)
+    lu = lu_no_pivot(A)
+    print(lu)
 
 
 if __name__ == "__main__":
